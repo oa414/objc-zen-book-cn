14849d69fdad6d7cb3af0531b128780d0ae249eb
Luca Bernardi <luka.bernardi@gmail.com>
2014-07-08 21:55:30 +0100

    First release

---
+++ scripts/make_toc.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+File.open("../objc-zen-book.md", 'r') do |f|
+  f.each_line do |line|
+    forbidden_words = [
+      'Table of contents', 
+      'define', 
+      'pragma', 
+      'Zen and the Art of the Objective-C Craftsmanship']
+      
+    next if !line.start_with?("#") || forbidden_words.any? { |w| line =~ /#{w}/ }
+     
+    title = line.gsub("#", "").strip
+    href = title.gsub(" ", "-").downcase
+    puts "  " * (line.count("#")-1) + "* [#{title}](\##{href})"
+  end
+end
